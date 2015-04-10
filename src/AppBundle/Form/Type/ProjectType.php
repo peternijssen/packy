3b0f833e0c232e84a2abd393504723e6bbe953ef
Peter Nijssen <peter@peternijssen.nl>
2015-04-11 00:39:55 +0200

    Fixed minor code issues

--- src/AppBundle/Form/Type/ProjectType.php
+++ src/AppBundle/Form/Type/ProjectType.php
@@ -15,9 +15,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-/**
- * @TODO: Validate
- */
 class ProjectType extends AbstractType
 {
     /**
