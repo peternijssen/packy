0dbdbe6148d0d6e8db0741935d8b968d0066d86a
Peter Nijssen <peter@peternijssen.nl>
2015-04-10 15:59:38 +0200

    improved code to get the latest stable version

--- src/AppBundle/Analyzer/ComposerAnalyzer.php
+++ src/AppBundle/Analyzer/ComposerAnalyzer.php
@@ -68,86 +68,15 @@ class ComposerAnalyzer implements AnalyzerInterface
     {
         $latestVersion = '0.0.0';
         foreach ($versions as $version) {
-            $nowVersion = $this->normalizeVersion($version);
-            if (version_compare($latestVersion, $nowVersion) >= 0) {
-                continue;
+            $version = ltrim($version, 'v');
+            if (preg_match('/^[0-9.]+$/', $version)) {
+                if (version_compare($latestVersion, $version) >= 0) {
+                    continue;
+                }
+                $latestVersion = $version;
             }
-            $latestVersion = $nowVersion;
         }
 
         return $latestVersion;
-    }
-
-    /**
-     * Normalize vendor version
-     *
-     * @param string $rawVersion
-     *
-     * @return string $version
-     */
-    public function normalizeVersion($rawVersion)
-    {
-        list($versionCandidate) = explode('-', $rawVersion);
-        $versionCandidate = str_replace('v', '', $versionCandidate);
-        if (strpos($versionCandidate, ',') !== false) {
-            $versionsRange = explode(',', $versionCandidate);
-            $versionCandidate = '0.0.0';
-            foreach ($versionsRange as $versionRange) {
-                $nowVersion = $this->determineVersionValue($versionRange);
-                if (version_compare($versionCandidate, $nowVersion) < 0) {
-                    $versionCandidate = $nowVersion;
-                }
-            }
-        } else {
-            $versionCandidate = $this->determineVersionValue($versionCandidate);
-        }
-
-        return $versionCandidate;
-    }
-
-    /**
-     * Determine version value and handle wildcards and comparison operator
-     *
-     * @param string $rawVersion
-     *
-     * @return string $version
-     */
-    private function determineVersionValue($rawVersion)
-    {
-        $version = str_replace(array('*', ' '), array('999', ''), $rawVersion);
-        if (preg_match('/^([\~\>\<\=\!]+)([0-9\.]+)$/', $version, $m) && count($m) == 3) {
-            $operator = $m[1];
-            $version = $m[2];
-            $versionAnnotations = explode('.', $version);
-            if (count($versionAnnotations) == 3) {
-                list($major, $minor, $patch) = $versionAnnotations;
-            } else {
-                switch (count($versionAnnotations)) {
-                    case 2:
-                        list($major, $minor) = $versionAnnotations;
-                        $patch = 999;
-                        break;
-
-                    default:
-                        $major = $versionAnnotations;
-                        $minor = 999;
-                        $patch = 999;
-                        break;
-                }
-            }
-            if (strpos($operator, '>') !== false || strpos($operator, '!') !== false || strpos($operator, '~') !== false) {
-                $version = $major.'.999.999';
-            } elseif (strpos($operator, '<') !== false) {
-                if ($major == 0 && $minor > 0) {
-                    $version = $major.'.'.(((int) $minor) - 1).'.999';
-                } elseif ($patch == 0) {
-                    $version = (((int) $major) - 1).'.999.999';
-                } else {
-                    $version = $major.'.0.0';
-                }
-            }
-        }
-
-        return $version;
     }
 }
